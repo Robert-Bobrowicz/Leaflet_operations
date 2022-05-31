95da800c3e890b223c57fb0cd5b1dcb08e140789
unknown <r.bobrowicz@live.com>
2022-05-31 18:20:06 +0200

    map operations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 
 function App() {
+    const mapInitialCoordinates = [52.231140, 21.004146];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Leaflet map operations
+        <MapContainer center={mapInitialCoordinates} zoom={15} scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={mapInitialCoordinates}>
+                <Popup>
+                    <h4>Marker text</h4> <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
     </div>
   );
 }
